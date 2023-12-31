522a2ff3ba694bd6a5ebac47ef15b3962c469ed2
Crowdbotics <team@crowdbotics.com>
2023-12-31 12:45:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-mud-45235",
+  url: "https://yellow-mud-45235.botics.co",
+  api: "https://yellow-mud-45235.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
